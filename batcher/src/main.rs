2a2159fcf79cb1f5a35868136eeaf5e7d3f71bf9
mationorato <onoratomatias@gmail.com>
2024-10-24 19:23:26 -0300

    add markle tree proof generation and verification demo

--- batcher/src/main.rs
+++ batcher/src/main.rs
@@ -1,18 +1,14 @@
 mod bills;
 mod merkle_tree;
 
-
-use lambdaworks_crypto::hash::poseidon::starknet::PoseidonCairoStark252;
-
-
-
 use merkle_tree::{
-    VerificationCommitmentBatch,
-    generate_merkle_tree
+    generate_merkle_proof, generate_merkle_tree, verify_merkle_proof
 };
-
-
 
 fn main() {
     generate_merkle_tree("../data/electricity_bills.json");
+    generate_merkle_proof("../data/electricity_bills.json", 0);
+    generate_merkle_proof("../data/electricity_bills.json", 1);
+    generate_merkle_proof("../data/electricity_bills.json", 2);
+    verify_merkle_proof();
 }
--- batcher/src/merkle_tree.rs
+++ batcher/src/merkle_tree.rs
@@ -40,10 +40,86 @@ pub fn generate_merkle_tree(tree_path: &str) -> Result<(), io::Error> {
     //let root = merkle_tree.root.representative().to_string();
     //println!("Generated merkle tree with root: {:?}", root);
 
-    let generated_tree_path = tree_path.replace(".json", ".merkle_tree.json").replace("data", "output");
+    let generated_tree_path = tree_path
+        .replace(".json", ".merkle_tree.json")
+        .replace("data", "output");
     let file = File::create(generated_tree_path)?;
     let mut writer = BufWriter::new(file);
     serde_json::to_writer_pretty(&mut writer, &merkle_tree)?;
     println!("Saved tree to file");
+    Ok(())
+}
+
+pub fn generate_merkle_proof(tree_path: &str, pos: usize) -> Result<(), io::Error> {
+    let values: Vec<Bill> = Bills::load_from_file(tree_path).unwrap().bills;
+    let merkle_tree = MerkleTree::<VerificationCommitmentBatch>::build(&values)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "requested empty tree"))?;
+
+    let Some(proof) = merkle_tree.get_proof_by_pos(pos) else {
+        return Err(io::Error::new(io::ErrorKind::Other, "Index out of bounds"));
+    };
+
+    let proof_path = tree_path
+        .replace(".json", format!(".merkle_proof.{pos}.json").as_str())
+        .replace("data", "output");
+        
+    let file = File::create(proof_path)?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer_pretty(&mut writer, &proof)?;
+    writer.flush()
+}
+
+pub fn verify_merkle_proof(
+
+) -> Result<(), io::Error> {
+    let root_hash: [u8; 32] = [
+        72,
+        181,
+        11,
+        206,
+        187,
+        135,
+        153,
+        115,
+        192,
+        78,
+        41,
+        169,
+        138,
+        138,
+        43,
+        12,
+        94,
+        7,
+        231,
+        146,
+        49,
+        230,
+        163,
+        253,
+        94,
+        181,
+        216,
+        189,
+        136,
+        29,
+        13,
+        29
+      ];
+
+    let leaf: Bill = Bill {
+        consumer_id: "141093".to_string(),
+        period: "2024-02".to_string(),
+        consumption_items: vec![],
+    };
+
+    let file_str = fs::read_to_string("../output/electricity_bills.merkle_proof.2.json")?;
+    let proof: Proof<[u8; 32]> = serde_json::from_str(&file_str)?;
+
+    match proof.verify::<VerificationCommitmentBatch>(&root_hash, 2, &leaf) {
+        true => println!("\x1b[32mMerkle proof verified succesfully\x1b[0m"),
+        false => println!("\x1b[31mMerkle proof failed verifying\x1b[0m"),
+    }
+
     Ok(())
 }
