d4258191de0a694c2771d51562d825c8daa17749
mationorato <onoratomatias@gmail.com>
2024-10-28 18:22:43 -0300

    finish merkle tree generation, proving and verification

--- batcher/src/main.rs
+++ batcher/src/main.rs
@@ -1,14 +1,31 @@
 mod bills;
 mod merkle_tree;
 
+use bills::{Bill, BillLoader, Bills};
 use merkle_tree::{
     generate_merkle_proof, generate_merkle_tree, verify_merkle_proof
 };
 
 fn main() {
-    generate_merkle_tree("../data/electricity_bills.json");
-    generate_merkle_proof("../data/electricity_bills.json", 0);
-    generate_merkle_proof("../data/electricity_bills.json", 1);
-    generate_merkle_proof("../data/electricity_bills.json", 2);
-    verify_merkle_proof();
+    let merkle_tree_input_path = "../data/electricity_bills_100.json";
+    let merkle_tree_output_path = "../output/merkle_tree.json";
+    let merkle_proof_output_path = "../output/merkle_proof_[n].json";
+    let bills: Vec<Bill> = Bills::load_from_file(merkle_tree_input_path).unwrap().bills;
+
+    let merkle_tree = generate_merkle_tree(merkle_tree_input_path,merkle_tree_output_path).unwrap();
+    
+    // Generate Proof for each bill
+    for (i,_bill) in bills.iter().enumerate() {
+        generate_merkle_proof(
+            merkle_proof_output_path.replace("[n]", i.to_string().as_str()).as_str(), &merkle_tree, i)
+            .unwrap();
+    }
+
+    // Veirify Proof for each bill
+    for (i, bill) in bills.iter().enumerate() {
+        let merkle_proof = verify_merkle_proof(
+            &merkle_tree.root, bill, i, merkle_proof_output_path.replace("[n]", i.to_string().as_str()).as_str())
+            .unwrap();
+        println!("Bill {} verified: {}", i, merkle_proof);
+    }
 }
